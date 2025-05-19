28317654cf4d5e31e70e864614b5945ccb413f3f
Do Huy Duc <dohuyduc.work@gmail.com>
2025-05-19 11:42:34 +0700

    Empty branch from v2 to serve as staging base

--- testing/utils.py
+++
@@ -1,8 +0,0 @@
-def make_test_artifact(artifact_type):
-    class _A(artifact_type):
-        def _get_path(self):
-            return self.uri
-        @property
-        def path(self):
-            return self.uri
-    return _A
